2eb598dc18204e3dbfcd9ec4adb2c51524983730
Damian Ogrodnik <d.ogrodnik96@gmail.com>
2021-02-13 16:08:50 +0100

    Updates

---
+++ precache-manifest.b2c4154b2eb001cac7a34b230a1b09ac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bc944ace11bc12deb93390d4ca0e21",
+    "url": "/product-catalog/index.html"
+  },
+  {
+    "revision": "1cabf1c4cb18a8f7927b",
+    "url": "/product-catalog/static/css/2.4af28dbb.chunk.css"
+  },
+  {
+    "revision": "1cabf1c4cb18a8f7927b",
+    "url": "/product-catalog/static/js/2.a2137963.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/product-catalog/static/js/2.a2137963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42d4221b64b10d3af48",
+    "url": "/product-catalog/static/js/main.35e14d09.chunk.js"
+  },
+  {
+    "revision": "5142d562590878cc5041",
+    "url": "/product-catalog/static/js/runtime-main.232e89be.js"
+  },
+  {
+    "revision": "7335ef809365b5fd9623cbe241e1fc5f",
+    "url": "/product-catalog/static/media/Check.7335ef80.svg"
+  },
+  {
+    "revision": "1f9315f458a07c149cf551488e058beb",
+    "url": "/product-catalog/static/media/Loupe.1f9315f4.svg"
+  },
+  {
+    "revision": "ed5a35d51fab8392ddd00a6e591ab87f",
+    "url": "/product-catalog/static/media/empty-icon.ed5a35d5.svg"
+  },
+  {
+    "revision": "78a9690073b2a1a4ecbf62b5b891847e",
+    "url": "/product-catalog/static/media/filled-star.78a96900.svg"
+  },
+  {
+    "revision": "fcf9f4a087c6184f5f9ca20ccf01aa86",
+    "url": "/product-catalog/static/media/loginCover.fcf9f4a0.png"
+  },
+  {
+    "revision": "44c89fc4ad0d6dd4b85752f70c8abcd3",
+    "url": "/product-catalog/static/media/logo.44c89fc4.svg"
+  },
+  {
+    "revision": "8c25c2b465c01dc68dcee1f4f4c54bb8",
+    "url": "/product-catalog/static/media/spinner.8c25c2b4.svg"
+  },
+  {
+    "revision": "2f01c39029a0b6a52b54b1e29c9ea9ef",
+    "url": "/product-catalog/static/media/star.2f01c390.svg"
+  }
+]);
